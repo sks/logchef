3c9e244d881189ccd639e8e1b423740b88ec95d1
Karan Sharma <hello@mrkaran.dev>
2025-04-16 15:18:39 +0530

    prep for initial release

--- internal/server/server.go
+++ internal/server/server.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/swagger" // Swagger handler
 
 	// Import generated docs (will be created after running swag init)
@@ -85,6 +86,9 @@ func New(opts ServerOptions) *Server {
 
 	// Add essential middleware.
 	app.Use(recover.New()) // Recover from panics.
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed, // Prioritize speed over maximum compression
+	})) // Compress responses
 
 	// Create the Server instance, injecting dependencies.
 	s := &Server{
