44c5753a6745fcfb88c3f515e75ea8f1ba3f5f38
Karan Sharma <hello@mrkaran.dev>
2025-04-30 11:38:32 +0530

    chore: fix logo

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/getting-started/quickstart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fquickstart.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/tutorials/nginx-logs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftutorials%2Fnginx-logs.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting-started/quickstart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Fquickstart.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/user-guide/query-interface.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fuser-guide%2Fquery-interface.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/tutorials/vector-otel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftutorials%2Fvector-otel.mdx&astroContentModuleFlag=true")]]);
 		
